c602d3e5e6c09955895d40c79fc2b44c6ccd4f3a
Aniokrait <itititcreclickhub@gmail.com>
2023-07-23 00:50:34 +0900

    modify showIndicator from remember to rememberSaveable

--- app/src/main/java/io/github/aniokrait/androidsandbox/ui/screen/TransitSourceScreen.kt
+++ app/src/main/java/io/github/aniokrait/androidsandbox/ui/screen/TransitSourceScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,7 +23,7 @@ import kotlin.reflect.KProperty
 fun TransitSourceScreen(
     transit: () -> Unit
 ) {
-    var showIndicator by remember { mutableStateOf(false) }
+    var showIndicator by rememberSaveable { mutableStateOf(false) }
 
     Column {
         Button(onClick = {
