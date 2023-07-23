1402d9f6924b8ac94c75a7f8bb6b2c6ee6f79d99
Aniokrait <itititcreclickhub@gmail.com>
2023-07-23 11:37:53 +0900

    modify showIndicator from remember to rememberSaveable

--- app/src/main/java/io/github/aniokrait/androidsandbox/ui/MyNavigationCompose.kt
+++ app/src/main/java/io/github/aniokrait/androidsandbox/ui/MyNavigationCompose.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
@@ -27,7 +28,8 @@ fun MyNavigationCompose(modifier: Modifier = Modifier) {
     ) {
         composable("transitSource") {
             val vm: TransitSourceViewModel = hiltViewModel()
-            val isLoadCompleted = remember { mutableStateOf(false) }
+            //Activityが破棄されても保持し続けるためにrememberではなくrememberSaveableで定義
+            val isLoadCompleted = rememberSaveable { mutableStateOf(false) }
             TransitSourceScreen(
                 transit = {
                     //navController.navigateではなく、BooleanのMutableStateを渡す
