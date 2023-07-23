e06ae7193dad66e554340f153848f84750ab0a3a
Aniokrait <itititcreclickhub@gmail.com>
2023-07-23 15:26:25 +0900

    add error handling

--- app/src/main/java/io/github/aniokrait/androidsandbox/ui/viewmodel/TransitSourceViewModel.kt
+++ app/src/main/java/io/github/aniokrait/androidsandbox/ui/viewmodel/TransitSourceViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,8 +18,14 @@ class TransitSourceViewModel @Inject constructor(): BaseViewModel() {
     ) {
         viewModelScope.launch(Dispatchers.IO) {
             //ここでIO処理
-            delay(5000)
-            //終わったら遷移
+            try {
+                delay(5000)
+            } catch (e: Exception) {
+                // throwするなりResultをreturnするなり適切にエラーハンドリングして
+                // 処理がここで終わるようにしてください
+            }
+
+            //IOが正常終了したら遷移
             withContext(Dispatchers.Main) {
                 //navController.navigateを実行するのではなく、MutableStateを更新する
                 isLoadCompleted.value = true
